43a522fc973e5e920dca5e539c3b5d90fe9912ae
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-06-27 23:00:55 -0700

    Set up JQuery

---
+++ src/components/section..ts
@@ -0,0 +1,24 @@
+// enum SectionType {
+//     Straight,
+//     Curve
+// }
+
+// class Section {
+//     private type: SectionType;
+//     constructor(type: SectionType) {
+//         this.type = type;
+//     }
+// }
+
+// abstract class Shape {
+
+//     origin: number[];
+
+//     abstract aaa();
+// }
+
+// class Straight extends Shape {
+// }
+
+// class Curve extends Shape {
+// }
