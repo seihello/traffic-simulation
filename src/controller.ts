1aa94e1fc6ccf79676ecf42cdaecc95d61d9628d
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-06-28 14:57:52 -0700

    Fix the blurry line

--- src/controller.ts
+++ src/controller.ts
@@ -1,6 +1,8 @@
 $(() => {
-    const canvas = $("#app").children("canvas")[0].getContext("2d")
-    console.log(canvas)
-    
+    const canvas = $("#app").children("canvas")[0].getContext("2d")!;
+    canvas.beginPath()
+    canvas.moveTo(50, 50)
+    canvas.lineTo(400, 200)
+    canvas.stroke()
 })
 
