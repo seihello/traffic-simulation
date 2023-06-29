b520418c6102461e506b0aea7da00a4c3da76b67
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-06-29 01:03:19 -0700

    Create Arc class and a sample shape

--- src/components/graphics.ts
+++ src/components/graphics.ts
@@ -37,8 +37,45 @@ export class Line extends GraphicElement {
     // }
 }
 
+export enum ArcAngle {
+    Left,
+    Right,
+    Up,
+    Down
+}
 
-// class Arc {
-//     center: Point = [0, 0]
-//     radius: number = 0
-// }
+export class Arc extends GraphicElement {
+    center: Point
+    radius: number
+    angle: ArcAngle
+
+    constructor(startPoint: Point, endPoint: Point, angle: ArcAngle) {
+        super(startPoint, endPoint)
+        this.angle = angle
+        const diameter = Math.floor(Math.sqrt(Math.pow(endPoint.x - startPoint.x, 2) + Math.pow(endPoint.y - startPoint.y, 2)))
+        this.length = Math.floor(diameter * 3.14 / 2)
+        this.radius = Math.floor(diameter / 2)
+        this.center = {x: Math.floor(startPoint.x + endPoint.x) / 2, y: Math.floor(startPoint.y + endPoint.y) / 2}
+        console.log(`length = ${this.length}`)
+    }
+
+    draw(context: CanvasRenderingContext2D): void {
+        context.beginPath()
+
+        let startAngle = 0, endAngle = 0
+        switch(this.angle) {
+            case ArcAngle.Right:
+                console.log("right")
+                startAngle = 1.5 * Math.PI
+                endAngle = 0.5  * Math.PI
+                break
+            case ArcAngle.Left:
+                console.log("left")
+                startAngle = 0.5 * Math.PI
+                endAngle = 1.5  * Math.PI
+                break
+        }
+        context.arc(this.center.x, this.center.y, this.radius, startAngle, endAngle)
+        context.stroke()
+    }
+}
--- src/controller.ts
+++ src/controller.ts
@@ -1,12 +1,13 @@
-import { GraphicElement, Line } from './components/graphics.js'
+import { GraphicElement, Line, Arc, ArcAngle } from './components/graphics.js'
 
 $(() => {
     const context: CanvasRenderingContext2D = $("#app").children("canvas")[0].getContext("2d")!;
 
     const graphicElements: GraphicElement[] = []
-    graphicElements.push(new Line({x: 100, y: 100}, {x: 200, y: 200}))
-    graphicElements.push(new Line({x: 200, y: 200}, {x: 1000, y: 400}))
-    graphicElements.push(new Line({x: 1000, y: 400}, {x: 100, y: 100}))
+    graphicElements.push(new Line({x: 200, y: 100}, {x: 1000, y: 100}))
+    graphicElements.push(new Arc({x: 1000, y: 100}, {x: 1000, y: 400}, ArcAngle.Right))
+    graphicElements.push(new Line({x: 1000, y: 400}, {x: 200, y: 400}))
+    graphicElements.push(new Arc({x: 200, y: 400}, {x: 200, y: 100}, ArcAngle.Left))
 
     for(const graphicElement of graphicElements) {
         graphicElement.draw(context)
